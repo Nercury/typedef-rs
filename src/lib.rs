64fc120e93bb8042bb9d6fdb7208d9e48b30c897
Nerijus Arlauskas <nercury@gmail.com>
2014-12-12 20:10:26 +0200

    Add for on github ribbon.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+/*!
+<a href="https://github.com/Nercury/typedef-rs">
+    <img style="position: absolute; top: 0; left: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_left_darkblue_121621.png" alt="Fork me on GitHub">
+</a>
+<style>.sidebar { margin-top: 53px }</style>
+*/
+
 //! TypeDef is used to identify and compare types, as well as print their names.
 //!
 //! If you do not need readable type name, you should use `TypeId`. This
