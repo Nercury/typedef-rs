a35fb4c466e6ec19392ee46466be7360c32cf4b1
Nerijus Arlauskas <nercury@gmail.com>
2016-06-27 18:13:15 +0300

    Fix ribbon.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 //! <a href="https://github.com/Nercury/typedef-rs">
-//! <img style="position: absolute; top: 0; left: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_left_darkblue_121621.png" alt="Fork me on GitHub">
+//! <img src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png" style="position: absolute; top: 0; right: 0; border: 0; margin-top: 55px" alt="Fork me on GitHub">
 //! </a>
-//! <style>.sidebar { margin-top: 53px }</style>
 //!
 
 //! TypeDef is used to identify and compare types, as well as print their names.
